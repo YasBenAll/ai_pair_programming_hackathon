17b3394e4602356a4a2241e830692d19d845f9de
Luuk van der Velden <luuk@codebeez.nl>
2023-01-12 21:11:56 +0100

    initial exercises

---
+++ copilot/1_fitness_instructions.py
@@ -0,0 +1,30 @@
+
+
+"""
+Compute the total intensity of repetitions of a workout.
+"""
+
+
+"""
+
+Compute the total intensity of a routine for any number of workouts.
+
+"""
+
+
+"""
+Compute the total intensity for any number of days that has any number routines. 
+"""
+
+
+"""
+Plan a week of workouts based on the different parts of the body
+and return the total intensity.
+"""
+
+
+"""
+Define a minimum and maximum weekly intensity and select a plan on intensity and 
+balance across the parts of the body and days of the week. 
+
+"""
---
+++ copilot/2_cooking.py
@@ -0,0 +1,15 @@
+"""
+Pick a favorite recipe.
+
+Ingredients and spices are required with quantities or weights.
+
+"""
+
+"""
+1. Store the ingredients and spices in a data structure.
+2. Make map for the associated quantities and weights.
+3. Create a recipe class.
+4. Create a generator to yield the ingredients and spices for each step.
+5. Wrap the generator to enrich it with the quantities and weights.
+6. Create a method to render the recipe as html. 
+"""
