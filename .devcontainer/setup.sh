9112f7de80673fd1a8cbe7edf9dfc66301f2a958
Luuk van der Velden <luuk@codebeez.nl>
2023-02-11 18:19:42 +0000

    add setup.sh to install github cli, poetry and run poetry install

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,9 @@
+type -p curl >/dev/null || apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& apt update \
+&& apt install gh -y
+pip install --no-cache-dir poetry==1.3.2 \
+&& poetry config virtualenvs.create false \
+&& poetry install
