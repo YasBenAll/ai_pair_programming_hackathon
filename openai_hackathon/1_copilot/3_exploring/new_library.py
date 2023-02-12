1ca70669ec09dc8824785386116109f75b0de9e5
Luuk van der Velden <luuk@codebeez.nl>
2023-02-12 15:44:50 +0000

    add computing libraries, exploring section and renames

---
+++ openai_hackathon/1_copilot/3_exploring/new_library.py
@@ -0,0 +1,17 @@
+"""
+Choose the library you are most unfamiliar with and complete the tasks.
+
+libraries:
+- pandas
+- numpy
+- xarray
+- polars
+"""
+
+"""
+Create an array of normal distributed random numbers with 1000 elements.
+With the following averages per column:
+- 0.5
+- 0.0
+- -0.5
+"""
